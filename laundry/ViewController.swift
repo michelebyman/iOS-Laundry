50852f0db981988e83c1cd32ecf66af1ea2b3034
Michele Byman <michelebyman@gmail.com>
2020-09-10 09:17:23 +0200

    new layout

--- laundry/ViewController.swift
+++ laundry/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 
